2ae9e1015d2293b1038e302856a8df58cc7d0f4b
SK3737 <swayamkotecha101@gmail.com>
2026-05-09 20:44:59 +0530

    Resolved some errors

--- init/index.js
+++ init/index.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Listing = require('../models/listing.js');
 
+const dbURL = process.env.ATLAS_URL || 'mongodb://127.0.0.1:27017/wanderlust';
+
 main()
-    .then((res) => {
+    .then(() => {
         console.log("connected to database");
     })
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(dbURL);
 }
 
 const initDB = async () => {
